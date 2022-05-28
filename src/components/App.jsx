c081a3fc4ea77aba48fa27ee64d7570990e205f5
MishynMykhailo <misham18092001@gmail.com>
2022-05-29 02:04:34 +0300

    try

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
+
 export class App extends Component {
   state = {
     contacts: [
@@ -11,36 +12,8 @@ export class App extends Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    name: '',
-    number: '',
   };
 
-  handlerChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-    console.log(this.state);
-  };
-
-  handlerSumbit = e => {
-    e.preventDefault();
-    this.addContacts();
-    this.reset();
-  };
-
-  addContacts = name => {
-    const contact = {
-      id: nanoid(),
-      name: this.state.name,
-      number: this.state.number,
-    };
-    this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts],
-    }));
-  };
-
-  reset = () => {
-    this.setState({ name: '', number: '' });
-  };
   changeFilter = e => {
     this.setState({ filter: e.currentTarget.value });
   };
@@ -50,35 +23,11 @@ export class App extends Component {
       return contact.name.toLowerCase().includes(normalizedFilter);
     });
 
-    console.log(visibleContacts);
     const { filter } = this.state;
     return (
       <div>
         <h1>Phonebook</h1>
-        <form onSubmit={this.handlerSumbit}>
-          <label>
-            Name
-            <input
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              onChange={this.handlerChange}
-              value={this.state.name}
-            />
-          </label>
-          <label>
-            Number
-            <input
-              type="tel"
-              name="number"
-              onChange={this.handlerChange}
-              value={this.state.number}
-            />
-          </label>
-          <button type="submit">Add contact </button>
-        </form>
+        <ContactForm />
         <Filter value={filter} onChange={this.changeFilter} />
         <h2>Contacts</h2>
         <ContactList contacts={visibleContacts} />
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,36 +1,61 @@
-// import { Component } from 'react';
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+export default class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
 
-// export class Form extends Component {
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-//   render() {
-//     return (
-//       <form onSubmit={this.handlerSumbit}>
-//         <label>
-//           Name
-//           <input
-//             type="text"
-//             name="name"
-//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//             required
-//             onChange={this.handlerChange}
-//             value={this.state.name}
-//           />
-//         </label>
-//         <label>
-//           Number
-//           <input
-//             type="tel"
-//             name="number"
-//             onChange={this.handlerChange}
-//             value={this.state.number}
-//           />
-//         </label>
-//         <button type="submit">Add contact </button>
-//       </form>
-//     );
-//   }
-// }
+  handlerChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handlerSumbit = e => {
+    e.preventDefault();
+    this.addContacts();
+    this.reset();
+  };
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+  addContacts = name => {
+    const contact = {
+      id: nanoid(),
+      name: this.state.name,
+      number: this.state.number,
+    };
+
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+  render() {
+    return (
+      <form onSubmit={this.handlerSumbit}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            onChange={this.handlerChange}
+            value={this.state.name}
+          />
+        </label>
+        <label>
+          Number
+          <input
+            type="tel"
+            name="number"
+            onChange={this.handlerChange}
+            value={this.state.number}
+          />
+        </label>
+        <button type="submit">Add contact </button>
+      </form>
+    );
+  }
+}
